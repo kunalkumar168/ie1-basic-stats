a614933b7549f2929305c4944318b78506fe07d0
Kunal Kumar <kunal.kumar.eee14@itbhu.ac.in>
2023-10-02 04:23:08 -0400

    Adding MinMaxCalculation.java

---
+++ src/MinMaxCalculation.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.*;
+
+
+public class MinMaxCalculation {
+    public static double getMin(ArrayList <Double> data)
+    {
+        return Collections.min(data);
+    }
+
+    public static double getMax(ArrayList <Double> data)
+    {
+        return Collections.max(data);
+    }
+}
